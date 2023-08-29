7ef2ab299bd289607a6540d143ed7007704f0093
sibowest <140107271+sibowest@users.noreply.github.com>
2023-08-29 15:12:38 +0200

    deploy

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// document.getElementById("demo").innerHTML="@West";
+// document.write('West test!');
+// window.alert('Warm Welcome to my Portfolio');
+// console.log('Warm Welcome to my Portfolio');
+function openNav(){
+    document.getElementById('myMenu').style.width="250px";
+}
+
+function closeNav(){
+    document.getElementById('myMenu').style.width="0";
+}
